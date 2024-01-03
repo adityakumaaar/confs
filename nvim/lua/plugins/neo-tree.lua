0de8ce006a6a2d87da388b86771b1189f40aded5
aditkum4 <aditkum4@cisco.com>
2024-01-03 12:38:45 +0530

    Initial Commit

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+	end,
+}
